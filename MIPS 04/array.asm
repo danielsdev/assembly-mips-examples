b6ae21b1fae8eccfaec2717004a653df74f7a394
Daniel Lima <danielrlima2015@gmail.com>
2022-10-20 12:12:41 -0300

    Fix UTF-8 encoding error

--- MIPS 04/array.asm
+++ MIPS 04/array.asm
@@ -8,7 +8,7 @@
      li $t2, 5            # coloca o indice em $t2
      add $t2, $t2, $t2    # indice x2
      add $t2, $t2, $t2    # indice x4
-     add $t1, $t2, $t3    # armazena em $t1 o endere�o do indice 5 de $t3
+     add $t1, $t2, $t3    # armazena em $t1 o endereço do indice 5 de $t3
        
     lw $t4, 36($t3)       # seleciona o indice do vetor
     
